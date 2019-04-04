1c9c780e99c3fa59e062f63dec183898beda89b6
Sheldon <sheldon.lim@online-pajak.com>
2019-04-04 10:20:20 +0800

    Convert Roman back to Numeral

--- src/RomanNumber.js
+++ src/RomanNumber.js
@@ -13,14 +13,30 @@ const numberToRomanTable = {
   4: "IV",
   1: "I"
 };
+const numeralKeys = Object.keys(numberToRomanTable).reverse();
 
-const keys = Object.keys(numberToRomanTable).reverse();
+const romanToNumberTable = {
+  "M": 1000,
+  "CM": 900,
+  "D": 500,
+  "CD": 400,
+  "C": 100,
+  "XC": 90,
+  "L": 50,
+  "XL": 40,
+  "X": 10,
+  "IX": 9,
+  "V": 5,
+  "IV": 4,
+  "I": 1
+};
+const romanKeys = Object.keys(romanToNumberTable).reverse();
 
-class RomanNumber {
-  perform(number) {
+class Convert {
+  numToRom(number) {
     let result = "";
 
-    keys.forEach(key => {
+    numeralKeys.forEach(key => {
       while (number >= parseInt(key)) {
         number -= parseInt(key);
         result += numberToRomanTable[key];
@@ -29,5 +45,26 @@ class RomanNumber {
 
     return result;
   }
+
+  romToNum(roman) {
+    let romanArray = roman.split('');
+    let numberArray = [];
+
+    romanArray.forEach(char => {
+      return romanKeys.forEach(key => {
+        if (key === char) {
+          numberArray.push(romanToNumberTable[key])
+        }
+      })
+    });
+
+    let number = numberArray.map((num, index, array) => {
+      if (num < array[index + 1])
+        return array[index] * -1;
+      else return num
+    })
+
+    return number.reduce((a, b) => a + b)
+  }
 }
-module.exports = new RomanNumber();
+module.exports = new Convert();
